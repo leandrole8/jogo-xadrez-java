986af892e56c433b71f66654a03974b4e5adc229
leandrole8 <leandro_le8@hotmail.com>
2020-05-22 12:14:22 -0300

    Implementação inicial das classes Board e Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Board {
+	
+	private int rowns;
+	private int columns;
+	private Piece[][] pieces;//matriz para as peças do tabuleiro
+	
+	public Board(int rowns, int columns) {
+		this.rowns = rowns;
+		this.columns = columns;
+		pieces = new Piece[rowns][columns];
+	}
+
+	public int getRowns() {
+		return rowns;
+	}
+
+	public void setRowns(int rowns) {
+		this.rowns = rowns;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+}
